3409a126015a8413a68a7a6de48ff1f169478340
Linus Jäderlund <linus@jaederlund.se>
2019-12-09 11:44:09 +0100

    Handling of default values in config build method

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const config = require('./lib/config');
   const arguments = process.argv.filter((item, index) => index > 1);
   // config
   const serverConfig = await config.build(arguments);
+
+  console.log(serverConfig);
 }());
 
