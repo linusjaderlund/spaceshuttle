fe6226a90ca7adc00eee0f3d21cb465955218023
Linus Jäderlund <linus@jaederlund.se>
2019-12-09 11:27:11 +0100

    Added build config method

---
+++ index.js
@@ -0,0 +1,10 @@
+// dependencies
+const config = require('./lib/config');
+
+(async function () {
+  // arguments
+  const arguments = process.argv.filter((item, index) => index > 1);
+  // config
+  const serverConfig = await config.build(arguments);
+}());
+
