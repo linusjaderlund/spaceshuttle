fca2cfbfa6f6ff6a0f8abe05c2a72754ba31f7fc
Linus Jäderlund <linus@jaederlund.se>
2019-12-09 18:17:14 +0100

    Bulk of it all

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // dependencies
 const config = require('./lib/config');
+const deploy = require('./lib/deploy');
 
 (async function () {
   // arguments
   const arguments = process.argv.filter((item, index) => index > 1);
   // config
   const serverConfig = await config.build(arguments);
-
-  console.log(serverConfig);
+  // start client
+  deploy.init(serverConfig);
 }());
 
